ba25961936580a4c2c2112781765f37efdef426d
yanab <yanaborja610@gmail.com>
2026-03-04 00:13:46 +0800

    Added game's appearance (e.g. colors, dimensions)

---
+++ game_settings.py
@@ -0,0 +1,14 @@
+# COLORS (r, g, b)
+white = (255, 255, 255)
+black = (0, 0, 0)
+sky_blue = (135, 206, 250)
+light_grey = (100, 100, 100)
+background_color = sky_blue
+
+# game settings
+width = 800
+height = 641
+fps = 60
+title = "Sliding Puzzle Game"
+tile_size = 128
+game_size = 3
