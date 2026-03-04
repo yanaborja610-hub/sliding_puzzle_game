7deb171eb49aebc5b3e32aa23aa0705f89e579b2
yanab <yanaborja610@gmail.com>
2026-03-04 22:16:12 +0800

    Error on the tile class

---
+++ game_components.py
@@ -0,0 +1,21 @@
+import pygame
+from game_settings import *
+
+pygame.font.init()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, text):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.Surface(tile_size, tile_size)
+        self.x, self.y = x, self.text = text
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.x = self.x * tile_size
+        self.rect.y = self.y * tile_size
+
+    def click(selfself,mouse_x, mouse_y):
+        return self.rect.left <= mouse_x <= self.rect.right and self.rect.top <= mouse_y <= self.rect.bottom
