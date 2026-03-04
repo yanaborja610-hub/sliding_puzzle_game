5f29ddf7364d3c0d213497788ece0c4fefe32276
yanab <yanaborja610@gmail.com>
2026-03-05 03:06:52 +0800

    Changed background color and some variable names

--- game_settings.py
+++ game_settings.py
@@ -1,13 +1,13 @@
 # COLORS (r, g, b)
 white = (255, 255, 255)
 black = (0, 0, 0)
-sky_blue = (135, 206, 250)
+pink = (255, 182, 193)
 light_grey = (100, 100, 100)
-background_color = sky_blue
+background_color = pink
 
 # game settings
-width = 800
-height = 641
+grid_width = 800
+grid_height = 641
 fps = 60
 title = "Sliding Puzzle Game"
 tile_size = 128
